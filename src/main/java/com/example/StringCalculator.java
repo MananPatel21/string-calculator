5bc5bfd165490cf470b8006c631c7281df2428cf
MananPatel21 <21ceuos018@ddu.ac.in>
2024-07-31 00:13:11 +0530

    Initial commit (Project setup & First test with empty String)

---
+++ src/main/java/com/example/StringCalculator.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class StringCalculator{
+    public static void main(String[] args) {
+        
+    }
+
+    public int add(String numbers) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testAddEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+}
