f503c631bc9d60b1afbd21ebf86a70d68eef8f72
MananPatel21 <21ceuos018@ddu.ac.in>
2024-07-31 00:21:12 +0530

    Testing for single number

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -1,11 +1,17 @@
 package com.example;
 
 public class StringCalculator{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         
     }
 
-    public int add(String numbers) {
-        return 0;
+    public int add(String numbers){
+
+        // Check and return 0 if string is empty
+        if(numbers.isEmpty()){
+            return 0;
+        }
+
+        return Integer.parseInt(numbers);
     }
 }
--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -5,9 +5,18 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
+    // Testing empty string
     @Test
     public void testAddEmptyString() {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
     }
+
+    // Testing for single number
+    @Test
+    public void testAddSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(21, calculator.add("21"));
+    }
+
 }
